2940e4ce08c5379929e7d7345e6ed946f357abe9
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-06 15:28:14 -0300

    Caso de uso de perfil

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, User } from '@prisma/client'
 
 export interface UsersRepository {
+  findById(email: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
