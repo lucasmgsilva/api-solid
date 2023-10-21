5f6231d7207a5251d1ce98953928be7d7a8be52c
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-10-20 23:04:59 -0300

    Teste E2E da autenticação

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { describe, beforeAll, afterAll, it, expect } from 'vitest'
+import { app } from '@/app'
+
+describe('Register (e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
