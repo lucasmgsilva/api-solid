a95348fc4f70be7c148a9fcf3d8c3f7741484c8b
Lucas Matheus <lucasmgsilva@hotmail.com>
2024-01-18 13:11:20 -0300

    Estratégia de refresh de token

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
 import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from '@/env'
-import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
+app.register(fastifyCors, {
+  origin: true,
+  credentials: true,
+})
+
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
